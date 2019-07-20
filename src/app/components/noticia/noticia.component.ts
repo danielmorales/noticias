db025be1067724b244c5a2daa0c7dace698ce46f
Daniel Morales <danielmorales.ag@gmail.com>
2019-07-20 18:06:42 -0400

    version pwa conectada a firebase, con share api

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from 'src/app/pages/interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-import { ActionSheetController } from '@ionic/angular';
+import { ActionSheetController, Platform } from '@ionic/angular';
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 import { DataLocalService } from '../../services/data-local.service';
 
@@ -20,7 +20,8 @@ export class NoticiaComponent implements OnInit {
   constructor(private iab: InAppBrowser,
               private actionSheetCtrl: ActionSheetController,
               private socialSharing: SocialSharing,
-              private datalocalService: DataLocalService) { }
+              private datalocalService: DataLocalService,
+              private platform: Platform) { }
 
   ngOnInit() {}
 
@@ -68,6 +69,9 @@ export class NoticiaComponent implements OnInit {
           cssClass: 'action-dark',
           handler: () => {
             console.log('Share clicked');
+
+            this.compartirNoticia();
+
             this.socialSharing.share(
               this.noticia.title,
               this.noticia.source.name,
@@ -88,6 +92,34 @@ export class NoticiaComponent implements OnInit {
         }]
     });
     await actionSheet.present();
+
+  }
+
+  compartirNoticia() {
+
+    if (this.platform.is('cordova')) {
+
+      this.socialSharing.share(
+        this.noticia.title,
+        this.noticia.source.name,
+        '',
+        this.noticia.url
+      );
+    } else {
+      // tslint:disable-next-line: no-string-literal
+      if (navigator['share']) {
+        // tslint:disable-next-line: no-string-literal
+        navigator['share']({
+            title: this.noticia.title,
+            text: this.noticia.description,
+            url: this.noticia.url,
+        })
+          .then(() => console.log('Successful share'))
+          .catch((error) => console.log('Error sharing', error));
+      } else {
+          console.log('No se pudo compartir porque no se soporta');
+      }
+    }
 
   }
 
